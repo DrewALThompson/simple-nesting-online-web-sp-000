e1a616afe5c1c1ec18f4294dd03d30e1a9c3dcd8
Drew Thompson <drewallynthompson@gmail.com>
2020-09-01 01:08:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,12 +87,6 @@ programmer_hash = {
  }
 }
 
-
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }    
 end
 
 def changing_alan
@@ -113,6 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay]
      
      
 end
